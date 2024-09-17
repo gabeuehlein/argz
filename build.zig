02428e6d287b87f654089935708f52703d951f98
Gabriel Uehlein <gabeuehlein@gmail.com>
2024-09-16 23:44:00 -0400

    Fixup build.zig.zon and temporarily remove the run-example step in build.zig

--- build.zig
+++ build.zig
@@ -23,25 +23,6 @@ pub fn build(b: *std.Build) void {
 
     b.getInstallStep().dependOn(&install_docs.step);
 
-    const example = b.step("run-example", "run an example");
-    const demo = b.option([]const u8, "example", "the example to run");
-    const mod = b.addModule("argz", .{ .root_source_file = b.path("src/root.zig"), .target = target, .optimize = optimize });
-    if (demo) |d| {
-        if (std.mem.eql(u8, d, "git")) {
-            const git_demo = b.addExecutable(.{
-                .name = "git-demo",
-                .root_source_file = b.path("examples/git.zig"),
-                .target = target,
-                .optimize = optimize,
-            });
-            git_demo.root_module.addImport("argz", mod);
-            example.dependOn(&git_demo.step);
-            const run_example = b.addRunArtifact(git_demo);
-            if (b.args) |args| run_example.addArgs(args);
-            example.dependOn(&run_example.step);
-        }
-    }
-
     const check_lib = b.addStaticLibrary(.{
         .name = "argz",
         .root_source_file = b.path("src/root.zig"),
