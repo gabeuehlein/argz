5a92d3f8e679cb4cedab8619cdb4b4f61479a2e0
Gabriel Uehlein <gabeuehlein@gmail.com>
2024-09-17 15:07:53 -0400

    Re-add run-example to build.zig
    This adds run-example back to build.zig. The examples, however, do not exist yet. I'll add them as I fix bugs and add the functionality needed for them to work.

--- build.zig
+++ build.zig
@@ -21,6 +21,23 @@ pub fn build(b: *std.Build) void {
         .install_subdir = "doc/argz",
     });
 
+    const Example = enum { echo, git, help };
+    const example = b.option([]const u8, "example", "the example to run");
+
+    const run_example = b.step("run-example", "run an example");
+    if (example) |example_str| {
+        const mod = b.addModule("argz", .{ .root_source_file = b.path("src/root.zig"), .target = target, .optimize = optimize });
+        const e = std.meta.stringToEnum(Example, example_str) orelse @panic("invalid example provided");
+        const exe = switch (e) {
+            .echo => b.addExecutable(.{ .name = "echo-demo", .root_source_file = b.path("examples/echo.zig"), .target = target, .optimize = optimize }),
+            .git => b.addExecutable(.{ .name = "git-demo", .root_source_file = b.path("examples/git.zig"), .target = target, .optimize = optimize }),
+            .help => b.addExecutable(.{ .name = "help-demo", .root_source_file = b.path("examples/help.zig"), .target = target, .optimize = optimize }),
+        };
+        exe.root_module.addImport("argz", mod);
+        const artifact = b.addRunArtifact(exe);
+        run_example.dependOn(&artifact.step);
+    }
+
     b.getInstallStep().dependOn(&install_docs.step);
 
     const check_lib = b.addStaticLibrary(.{
